3a6f17d8ad4cdcc3b5956ea584d11989d465c3f6
OlofNymanson <onymanson@gmail.com>
2019-05-28 09:29:56 +0200

    example CRUD operations in MongoDB

---
+++ exampleCRUD/Location.java
@@ -0,0 +1,13 @@
+package exampleCRUD;
+
+public class Location {
+
+	public String address, city, id;
+	public Person resident;
+	
+	public Location(String address, String city, String id) {
+		this.address = address;
+		this.city = city;
+		this.id = id;
+	}
+}
---
+++ exampleCRUD/Person.java
@@ -0,0 +1,14 @@
+package exampleCRUD;
+
+public class Person {
+	
+	public String name, id;
+	public int age;
+	
+	public Person(String name, int age, String id) {
+		this.name = name;
+		this.age = age;
+		this.id = id;
+	}
+	
+}
